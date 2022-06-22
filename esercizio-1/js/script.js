169d21e3da05bb8fc5981de0826d71cc53ba4611
LucaSpennato <luca.spennato@gmail.com>
2022-06-22 16:32:08 +0200

    saved in second array and added, dont work as req

--- esercizio-1/js/script.js
+++ esercizio-1/js/script.js
@@ -15,13 +15,28 @@ function palindromeTester(wordToTest){
     // let ciao = 'ciao'; console.log(ciao[0]); risultato = c!!!!!
 
     // in un for, riordino la parola al contrario, eseguo quindi il for con i--
+    // cosa gli stiamo dicendo? Gli diciamo di partire dall'ultimo elemento dell''''''array''''' della stringa
+    // successivamente, gli diciamo di decrescere index-- e di fermarsi a 0. NB: >0 O SI FERMERà AL NUMERO 1 E TE MANCA UN ELEMENTO
+    let sum = [];
     for (let index = wordToTest.length-1; index >= 0; index--) {
         const element = wordToTest[index];
-        console.log(element);
+        // console.log(element);
+
+        sum.push(wordToTest[index]);
         
     }
     // salvo le lettere in un secondo array
     // unisco le parole in un'unica stringa
+    // console.log(sum);
+    let element;
+    for (let index = wordToTest.length-1; index >= 0; index--) {
+        element = `${sum} ${sum[index]}`;
+
+        
+    }
+    console.log(element);
+    
+    
     // faccio una comparazione tra la parola iniziale nel primo array ed il secondo
     // se la parola è uguale, return di 'è palindroma', oppure return di 'true' da con altro if
 
